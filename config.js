a7a3d7a39d482f06cf752b5405fc310aee808000
DansilvDEV <instagramjapinha1201@gmail.com>
2026-06-09 08:51:33 -0300

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// ===== CONFIGURAÇÃO SUPABASE =====
-// IMPORTANTE: Este arquivo NÃO deve ir para o GitHub (está no .gitignore)
-// Faça o upload manual deste arquivo direto no GitHub Pages
-
-const SUPABASE_URL = "https://oslkqesroakoltfcirjy.supabase.co";
-const SUPABASE_KEY = "sb_publishable_C1EXoWc7ly_-hHZALVWArw_7X3peuyx";
